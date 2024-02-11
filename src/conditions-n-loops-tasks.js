1fc74367a3800c09bc51169b485b7f63a333f67f
Glib Shemenkov <shemenkovglib@gmail.com>
2024-02-11 09:14:01 +0100

    feat: half test

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -110,8 +110,29 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  const romanNumerals = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+  };
+  const big = {
+    1: 'X',
+    2: 'XX',
+    3: 'XXX',
+  };
+  let romNum = '';
+  if (num > 9) {
+    romNum += big[Math.floor(num / 10)];
+  }
+  if (num % 10 !== 0) romNum += romanNumerals[num % 10];
+  return romNum;
 }
 
 /**
@@ -129,8 +150,61 @@ function convertToRomanNumerals(/* num */) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  let str = '';
+
+  for (let i = 0; i < numberStr.length; i += 1) {
+    const char = numberStr[i];
+    if (i !== numberStr.length && i !== 0) {
+      str += ' ';
+    }
+    switch (char) {
+      case '0':
+        str += 'zero';
+        break;
+      case '1':
+        str += 'one';
+        break;
+      case '2':
+        str += 'two';
+        break;
+      case '3':
+        str += 'three';
+        break;
+      case '4':
+        str += 'four';
+        break;
+      case '5':
+        str += 'five';
+        break;
+      case '6':
+        str += 'six';
+        break;
+      case '7':
+        str += 'seven';
+        break;
+      case '8':
+        str += 'eight';
+        break;
+      case '9':
+        str += 'nine';
+        break;
+      case '+':
+        str += 'plus';
+        break;
+      case '-':
+        str += 'minus';
+        break;
+      case ',':
+      case '.':
+        str += 'point';
+        break;
+      default:
+        break;
+    }
+  }
+
+  return str;
 }
 
 /**
@@ -145,8 +219,12 @@ function convertNumberToString(/* numberStr */) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    newStr += str[i];
+  }
+  return str === newStr;
 }
 
 /**
@@ -163,8 +241,16 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let num = 0;
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      num = i;
+      break;
+    } else num = -1;
+  }
+  return num;
 }
 
 /**
@@ -182,8 +268,13 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let numCheck = num;
+  while (numCheck > 0) {
+    if (numCheck % 10 === digit) return true;
+    numCheck = Math.floor(numCheck / 10);
+  }
+  return false;
 }
 
 /**
@@ -199,8 +290,25 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  if (arr.length < 3) return -1;
+  for (let i = 1; i < arr.length - 1; i += 1) {
+    let leftSum = 0;
+    let rightSum = 0;
+
+    for (let j = 0; j < i; j += 1) {
+      leftSum += arr[j];
+    }
+
+    for (let k = i + 1; k < arr.length; k += 1) {
+      rightSum += arr[k];
+    }
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -261,8 +369,20 @@ function rotateMatrix(/* matrix */) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const newArr = arr;
+  for (let i = 0; i < newArr.length; i += 1) {
+    let lessIndexNum = i;
+    for (let j = i + 1; j < newArr.length; j += 1) {
+      if (newArr[j] < newArr[lessIndexNum]) {
+        lessIndexNum = j;
+      }
+    }
+    const tmp = newArr[i];
+    newArr[i] = newArr[lessIndexNum];
+    newArr[lessIndexNum] = tmp;
+  }
+  return newArr;
 }
 
 /**
